a6ba1bbe3e3f3181103206020421fdfcf9a276fc
Gemille Ford <gemille.ford@manheim.com>
2016-06-30 17:07:10 -0400

    store movie years in an array

--- The_Basics/Strings/movie_hash.rb
+++ The_Basics/Strings/movie_hash.rb
@@ -1,8 +1,13 @@
 movies_hash = {
-  "The Matrix" => "1999", 
-  "Beaches" => "1988",
-  "Creed" => "2015", 
-  "The Dark Knight" => "2012"
+  'The Matrix' => '1999',
+  'Beaches' => '1988',
+  'Creed' => '2015',
+  'The Dark Knight' => '2012'
 }
 
-movies_hash.each_value { |year| puts year }
+movies_array = []
+
+movies_hash.each_value do |year|
+  puts year
+  movies_array << year
+end
