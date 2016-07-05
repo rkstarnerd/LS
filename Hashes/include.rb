91272d984cd53de667395a326ddfed717ba58159
Gemille Ford <gemille.ford@manheim.com>
2016-07-04 21:37:06 -0400

    Demonstrate Hash methods has_key? and has_value?
    https://launchschool.com/books/ruby/read/hashes#exercises
    Ex. 5

--- Hashes/include.rb
+++ Hashes/include.rb
@@ -1,8 +1,10 @@
 # Whas method could you use to find out if a Hash contains a specific value?
-# Answer: include?
+# Answer: include?, hash_key?, has_value?
 # Write a program to demonstrate this use.
 
 letters = ('a'..'g').to_a
 numbers = (1..7).to_a
 hash = Hash[letters.zip(numbers)]
 puts 'Did I get it?' if hash.include? 'g'
+puts 'I think I got it' if hash.has_key? 'f'
+puts ';)' if hash.has_value? 7
