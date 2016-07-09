83508fec716488276efa5b467dc979e4384fd2a6
Gemille Ford <gemille.ford@manheim.com>
2016-07-09 19:57:53 -0400

    Refactor
    1. Add top-level class documentation comment.
    2. Remove trailing whitespace
    3. Remove unnecessary spacing
    4. Change double quotes to single-quoted strings where
        string interpoloation is not needed.
    5. Add missing space around operator.

--- Exercises/Easy_2/room.rb
+++ Exercises/Easy_2/room.rb
@@ -1,6 +1,9 @@
-class Room 
+# The Room class has two methods:
+# (1) initialize - creates @length and @width attributes
+# (2) area - calculates a rooms area in meters and feet
+class Room
   attr_writer :length, :width
-  attr_reader   :area
+  attr_reader :area
 
   def initialize
     @length
@@ -18,9 +21,9 @@ end
 
 room = Room.new
 
-puts "What is the length of the room in meters?"
+puts 'What is the length of the room in meters?'
 room.length = gets.chomp.to_i
-puts "What is the width of the room in meters?"
-room.width= gets.chomp.to_i
+puts 'What is the width of the room in meters?'
+room.width = gets.chomp.to_i
 
 room.area
