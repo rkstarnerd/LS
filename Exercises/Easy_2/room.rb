b86174a85d7f7c7f197ee354ec7ea9af68be712a
Gemille Ford <gemille.ford@manheim.com>
2016-07-09 18:57:04 -0400

    How big is the room?
    Build a program that asks a user for the length and width of a room
    in meters and then displays the area of the room in both
    square meters and square feet.
    
    Note: 1 square meter == 10.7639 square feet
    
    Do not worry about validating the input at this time.
    
    https://launchschool.com/exercises/7fc5d216

---
+++ Exercises/Easy_2/room.rb
@@ -0,0 +1,26 @@
+class Room 
+  attr_writer :length, :width
+  attr_reader   :area
+
+  def initialize
+    @length
+    @width
+  end
+
+  def area
+    meters = @length * @width
+    feet = meters * 10.7639
+
+    puts "The area in square meters is #{meters}."
+    puts "The area in square feet is #{feet}."
+  end
+end
+
+room = Room.new
+
+puts "What is the length of the room in meters?"
+room.length = gets.chomp.to_i
+puts "What is the width of the room in meters?"
+room.width= gets.chomp.to_i
+
+room.area
