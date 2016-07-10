a1685163a3a6647ef19a7974fda14daf40217d3f
Gemille Ford <gemille.ford@manheim.com>
2016-07-10 17:35:31 -0400

    Refactor
    Use the String instance method size instead of converting the
    string to an array of characters and counting the characters.

--- Exercises/Easy_3/count_char.rb
+++ Exercises/Easy_3/count_char.rb
@@ -1,7 +1,6 @@
 puts 'Please write word or multiple words:'
 words = gets.chomp
 
-words_no_spaces = words.delete(' ')
-num_of_char = words_no_spaces.chars.count
+num_of_char = words.delete(' ').size
 
 puts "There are #{num_of_char} characters in \"#{words}\". "
